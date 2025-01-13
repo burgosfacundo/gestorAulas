abb6256cd606282a57eea99b9f20def1f6479c09
burgosfacundo <facundoandresbur@hotmail.com>
2025-01-13 11:50:56 -0300

    Add class main

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import org.example.utils.VistaUtils;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.io.IOException;
+
+
+@SpringBootApplication
+public class App extends Application {
+
+    private ConfigurableApplicationContext springContext;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void init() {
+        springContext = new SpringApplicationBuilder(App.class)
+                .run(getParameters().getRaw().toArray(new String[0]));
+    }
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            VistaUtils vistaUtils = springContext.getBean(VistaUtils.class);
+            vistaUtils.cargarVista("/org/example/view/menu-inicio-view.fxml");
+        } catch (IOException e) {
+            System.err.println("Error al cargar la vista inicial:");
+            e.printStackTrace();
+            Platform.exit();
+        }
+    }
+
+    @Override
+    public void stop() {
+        springContext.close();
+        Platform.exit();
+    }
+}
