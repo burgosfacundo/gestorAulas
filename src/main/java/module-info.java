79abe8ef0ee549ffa46ab3a1324f1a12106943cd
burgosfacundo <facundoandresbur@hotmail.com>
2025-01-27 17:45:24 -0300

    Add slf4j to module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module org.example.gestoraulas {
+    requires org.slf4j;
     requires javafx.fxml;
     requires org.controlsfx.controls;
     requires com.google.gson;
@@ -16,7 +17,7 @@ module org.example.gestoraulas {
     opens org.example.model to com.google.gson, javafx.base;
     opens org.example.model.dto to com.google.gson;
     opens org.example.enums to com.google.gson;
-    opens org.example.controller to javafx.fxml,spring.core,spring.context;
+    opens org.example.controller to javafx.fxml,spring.core,spring.context,org.slf4j;
     opens org.example.config to spring.context,spring.core,spring.beans;
     opens org.example.utils to spring.beans, spring.context, spring.core;
     opens org.example.security to spring.beans, spring.context, spring.core;
