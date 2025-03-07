9f89131eb94f3ae53e14419a8ff1745763f9e719
burgosfacundo <facundoandresbur@hotmail.com>
2025-03-07 15:20:40 -0300

    Add espacios menu

--- src/main/java/org/example/controller/menus/MenuEspacioVistaController.java
+++ src/main/java/org/example/controller/menus/MenuEspacioVistaController.java
@@ -5,11 +5,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.controller.model.AulaVistaController;
-import org.example.controller.model.LaboratorioVistaController;
-import org.example.exception.JsonNotFoundException;
-import org.example.security.SesionActual;
-import org.example.service.AulaService;
 import org.example.utils.VistaUtils;
 import org.springframework.stereotype.Component;
 
@@ -20,69 +15,47 @@ import java.io.IOException;
 @Component
 public class MenuEspacioVistaController {
     private final VistaUtils vistaUtils;
-    private final AulaService aulaService;
-    private final SesionActual sesionActual;
     @FXML
-    private Button btnListarLaboratorios ;
+    private Button btnListar;
     @FXML
-    private Button btnFiltrarAulas;
+    private Button btnCrear;
     @FXML
-    private Button btnListarAulas;
+    private Button btnEditar;
     @FXML
-    private Button btnFiltrarLaboratorios;
+    private Button btnEliminar;
     @FXML
     private Button btnVolver;
 
     @FXML
-    public void listarAulas(ActionEvent actionEvent) {
-        try {
-            var aulas = aulaService.listarAulas();
-            vistaUtils.cargarVista("/org/example/view/model/aula-view.fxml",
-                    (AulaVistaController controller) -> controller.setAulas(aulas));
-        } catch (IOException | JsonNotFoundException e) {
+    public void menuListar(ActionEvent actionEvent) {
+        try{
+            vistaUtils.cargarVista("/org/example/view/menus/menu-listar-espacios-view.fxml");
+            vistaUtils.cerrarVentana(this.btnVolver);
+        }catch (IOException e){
             log.error(e.getMessage());
         }
     }
 
     @FXML
-    public void listarLaboratorios(ActionEvent actionEvent) {
-        try {
-            var laboratorios = aulaService.listarLaboratorios();
-            vistaUtils.cargarVista("/org/example/view/model/laboratorio-view.fxml",
-                    (LaboratorioVistaController controller) -> controller.setLaboratorios(laboratorios));
-        } catch (IOException | JsonNotFoundException e) {
-            log.error(e.getMessage());
-        }
+    public void crearEspacio(ActionEvent actionEvent) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @FXML
-    public void filtrarAulasDisponibles(ActionEvent actionEvent) {
-        try {
-            vistaUtils.cargarVista("/org/example/view/filtros/aula-disponibles-view.fxml");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
+    public void editarEspacio(ActionEvent actionEvent) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @FXML
-    public void filtrarLaboratoriosDisponibles(ActionEvent actionEvent) {
-        try {
-            vistaUtils.cargarVista("/org/example/view/filtros/laboratorio-disponibles-view.fxml");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
+    public void eliminarEspacio(ActionEvent actionEvent) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @FXML
     public void volverMenuPrincipal(ActionEvent actionEvent) {
-        try {
-            if (sesionActual.getUsuario().getRol().getNombre().equals("Administrador")) {
-                vistaUtils.cargarVista("/org/example/view/menus/menu-admin-view.fxml");
-            }else if (sesionActual.getUsuario().getRol().getNombre().equals("Profesor")) {
-                vistaUtils.cargarVista("/org/example/view/menus/menu-profesor-view.fxml");
-            }
-
-        } catch (IOException e) {
+        try{
+            vistaUtils.cargarVista("/org/example/view/menus/menu-administrador-view.fxml");
+        }catch (IOException e){
             log.error(e.getMessage());
         }
         vistaUtils.cerrarVentana(this.btnVolver);
