b1d1e18c1f664e6b976620f77fb911534007154c
burgosfacundo <facundoandresbur@hotmail.com>
2025-03-30 17:53:05 -0300

    Change exception throw in method autenticar

--- src/main/java/org/example/security/Seguridad.java
+++ src/main/java/org/example/security/Seguridad.java
@@ -2,14 +2,13 @@ package org.example.security;
 
 
 import org.example.enums.Permisos;
-import org.example.exception.AutenticacionException;
-import org.example.exception.JsonNotFoundException;
-import org.example.exception.NotFoundException;
 import org.example.model.Usuario;
 import org.example.service.UsuarioService;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.naming.AuthenticationException;
 
 /**
  * Clase de seguridad de la app
@@ -30,16 +29,15 @@ public class Seguridad {
      * @param username nombre de usuario
      * @param password contraseña
      * @return Usuario que inicia sesión
-     * @throws AutenticacionException si el username o password son incorrectos
-     * @throws JsonNotFoundException si no se encuentra el archivo JSON
+     * @throws AuthenticationException si el username o password son incorrectos
      */
-    public Usuario autenticar(String username, String password) throws AutenticacionException, JsonNotFoundException, NotFoundException {
-        //Verificamos si existe un usuario con ese username y password a traves de un stream
+    public Usuario autenticar(String username, String password) throws AuthenticationException {
+        //Verificamos si existe un usuario con ese username y password a través de un stream
         return usuarioService.listar().stream()
                 .filter(usuario -> usuario.getUsername().equals(username) &&
                         BCrypt.checkpw(password, usuario.getPassword())) // decodifico la contraseña para validar
                 .findFirst()
-                .orElseThrow(() -> new AutenticacionException("Usuario o contraseña incorrecto."));
+                .orElseThrow(() -> new AuthenticationException("Usuario o contraseña incorrecto."));
     }
 
     /**
