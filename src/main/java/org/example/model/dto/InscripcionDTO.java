9ab8ea1df27eb14c2ec9825b9f72ffcfb1d7dc3e
burgosfacundo <facundoandresbur@hotmail.com>
2025-03-30 17:39:43 -0300

    Change dto and Mapper's logic

--- src/main/java/org/example/model/dto/InscripcionDTO.java
+++ src/main/java/org/example/model/dto/InscripcionDTO.java
@@ -3,5 +3,5 @@ package org.example.model.dto;
 import java.time.LocalDate;
 
 public record InscripcionDTO(Integer id, int cantidadAlumnos, int margenAlumnos, LocalDate fechaFinInscripcion,
-                             int idAsignatura, String comision, int idProfesor) {
+                             int idAsignatura, int comision, int idProfesor) {
 }
--- src/main/java/org/example/utils/Mapper.java
+++ src/main/java/org/example/utils/Mapper.java
@@ -6,9 +6,12 @@ import org.example.model.dto.InscripcionDTO;
 import org.example.model.dto.ReservaDTO;
 import org.example.model.dto.SolicitudCambioAulaDTO;
 import org.example.model.dto.UsuarioDTO;
+import org.mindrot.jbcrypt.BCrypt;
+
 
 @UtilityClass
 public class Mapper {
+
     //Usuario
 
     /**
@@ -16,12 +19,18 @@ public class Mapper {
      * @param dto que queremos mapear
      * @return Usuario mapeado desde dto
      */
-    public Usuario toUsuario(UsuarioDTO dto, Rol rol, Profesor profesor){
+    public Usuario toUsuario(UsuarioDTO dto){
         //Retornamos el usuario mapeado desde DTO, incluyendo su rol y el profesor que representa
+        var rol = new Rol();
+        rol.setId(dto.idRol());
+
+        var profesor = new Profesor();
+        profesor.setId(dto.idProfesor());
+
         return new Usuario(
                 dto.id(),
                 dto.username(),
-                dto.password(),
+                BCrypt.hashpw(dto.password(), BCrypt.gensalt()),
                 rol,
                 profesor
         );
@@ -50,8 +59,13 @@ public class Mapper {
      * @param dto que queremos mapear
      * @return Inscripción mapeado desde dto
      */
-    public Inscripcion toInscripcion(InscripcionDTO dto, Asignatura asignatura, Profesor profesor){
+    public Inscripcion toInscripcion(InscripcionDTO dto){
         //Retornamos la inscripción mapeando desde DTO, incluyendo su asignatura y profesor
+        var asignatura = new Asignatura();
+        asignatura.setId(dto.idAsignatura());
+
+        var profesor = new Profesor();
+        profesor.setId(dto.idProfesor());
         return new Inscripcion(
                 dto.id(),
                 dto.cantidadAlumnos(),
@@ -89,8 +103,13 @@ public class Mapper {
      * @param dto que queremos mapear
      * @return Inscripción mapeado desde dto
      */
-    public Reserva toReserva(ReservaDTO dto, Aula aula, Inscripcion inscripcion){
-        //Retornamos la inscripción mapeando desde DTO, incluyendo su asignatura y profesor
+    public Reserva toReserva(ReservaDTO dto){
+        var aula = new Aula();
+        aula.setId(dto.idEspacio());
+
+        var inscripcion = new Inscripcion();
+        inscripcion.setId(dto.idInscripcion());
+
         return new Reserva(
                 dto.id(),
                 dto.fechaInicio(),
@@ -112,7 +131,7 @@ public class Mapper {
                 reserva.getId(),
                 reserva.getFechaInicio(),
                 reserva.getFechaFin(),
-                reserva.getAula().getId(),
+                reserva.getEspacio().getId(),
                 reserva.getInscripcion().getId(),
                 reserva.getDiasYBloques()
         );
@@ -127,12 +146,20 @@ public class Mapper {
      * @param dto que queremos mapear
      * @return SolicitudCambioAula mapeado desde DTO
      */
-    public SolicitudCambioAula toSolicitud(SolicitudCambioAulaDTO dto, Profesor profesor, Aula aula,
-                                            Reserva reserva){
+    public SolicitudCambioAula toSolicitud(SolicitudCambioAulaDTO dto){
         //Retornamos la solicitud mapeando desde DTO, incluyendo su aula, profesor y reserva original
+        var aula = new Aula();
+        aula.setId(dto.idEspacio());
+
+        var profesor = new Profesor();
+        profesor.setId(dto.idProfesor());
+
+        var reserva = new Reserva();
+        reserva.setId(dto.idReserva());
+
         return new SolicitudCambioAula(
                 dto.id(), profesor, reserva, aula, dto.estadoSolicitud(), dto.tipoSolicitud(),
-                dto.fechaInicio(), dto.fechaFin(), dto.diasYBloques(),
+                dto.fechaInicio(), dto.fechaFin(),dto.diasYBloques(),
                 dto.comentarioEstado(), dto.comentarioProfesor(), dto.fechaHoraSolicitud()
         );
     }
@@ -146,7 +173,7 @@ public class Mapper {
         //Retornamos el DTO mapeado desde SolicitudCambioAula
         return new SolicitudCambioAulaDTO(
                 solicitud.getId(), solicitud.getProfesor().getId(), solicitud.getReservaOriginal().getId(),
-                solicitud.getNuevaAula().getId(), solicitud.getEstado(), solicitud.getTipoSolicitud(),
+                solicitud.getNuevoEspacio().getId(), solicitud.getEstado(), solicitud.getTipoSolicitud(),
                 solicitud.getFechaInicio(), solicitud.getFechaFin(), solicitud.getDiasYBloques(),
                 solicitud.getComentarioEstado(), solicitud.getComentarioProfesor(), solicitud.getFechaHoraSolicitud()
         );
