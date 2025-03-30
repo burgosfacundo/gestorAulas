8e2ac099f2004618789a884a11784f27b92a35ce
burgosfacundo <facundoandresbur@hotmail.com>
2025-03-30 17:34:33 -0300

    Change to JpaRepository

---
+++ src/main/java/org/example/repository/DiaBloqueRepository.java
@@ -0,0 +1,7 @@
+package org.example.repository;
+
+import org.example.model.DiaBloque;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiaBloqueRepository extends JpaRepository<DiaBloque, Long> {
+}
