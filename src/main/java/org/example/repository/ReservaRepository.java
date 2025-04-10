6c1ce054fd42def348ff8fe3e1786ea6542b017e
burgosfacundo <facundoandresbur@hotmail.com>
2025-04-10 19:15:30 -0300

    Add method findReservaByEspacio

--- src/main/java/org/example/repository/ReservaRepository.java
+++ src/main/java/org/example/repository/ReservaRepository.java
@@ -2,6 +2,7 @@ package org.example.repository;
 
 
 import org.example.model.DiaBloque;
+import org.example.model.Espacio;
 import org.example.model.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -30,5 +31,7 @@ public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
             @Param("fechaInicio") LocalDate fechaInicio,
             @Param("fechaFin") LocalDate fechaFin,
             @Param("diasYBloques") Set<DiaBloque> diasYBloques);
+
+    List<Reserva> findReservaByEspacio(Espacio espacio);
 
 }
